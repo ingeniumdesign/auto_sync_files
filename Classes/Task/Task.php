fa69ade872258c9fbb91993a38e9419e740ec264
felixmaechtle <f.maechtle@ingeniumdesign.de>
2018-06-05 15:14:46 +0200

    Fixed a minor bug that prevented the Extension from working on some servers

--- Classes/Task/Task.php
+++ Classes/Task/Task.php
@@ -8,6 +8,11 @@ class Task extends \TYPO3\CMS\Scheduler\Task\AbstractTask
     {
         // Download File
         $newFile = file_get_contents($this->auto_sync_files_file_url);
+
+        if (!file_exists($this->auto_sync_files_local_path)) {
+            file_put_contents($this->auto_sync_files_local_path, '');
+        }
+
         $oldFile = file_get_contents($this->auto_sync_files_local_path);
 
         if ($newFile === $oldFile) {
